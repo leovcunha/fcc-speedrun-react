6cbab2a8390210940e50995028c687c8064ff8ec
leovcunha <lvcunha@gmail.com>
2018-05-25 07:55:43 -0300

    calc wiring

--- src/frontend/calculator/index.js
+++ src/frontend/calculator/index.js
@@ -19,6 +19,40 @@ export default class Calculator extends Component {
             <div className="calculator jumbotron">
                 <div className="row h1 justify-content-center">Calculator</div>
                 <hr className="p-5"/>
+                <div className="calc">
+                    <table>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>+</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>5</td>
+                            <td>6</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>7</td>
+                            <td>8</td>
+                            <td>9</td>
+                            <td>x</td>
+                        </tr>
+                        <tr>
+                            <td>0</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
 
             </div>
         );
